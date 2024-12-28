a7ecc404f83f9f6a8882aaddd809f598b1444538
brooksolomon <officialbrooksolo@gmail.com>
2024-12-28 18:46:23 +0300

    dummy for all added

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -22,7 +22,80 @@ export default function Welcome({
     <>
       <DefaultLayout>
         <Head title='Welcome' />
+        <BarChartC
+          title='Line Chart - Single'
+          timeframe='January - June 2024'
+          chartData={[
+            { month: "January", desktop: 186, mobile: 93 },
+            { month: "February", desktop: 305, mobile: 552 },
+            { month: "March", desktop: 237, mobile: 118 },
+            { month: "April", desktop: 73, mobile: 36 },
+            { month: "May", desktop: 209, mobile: 104 },
+            { month: "June", desktop: 214, mobile: 107 },
+          ]}
+          chartConfig={{
+            desktop: {
+              label: "Desktop",
+              color: "hsl(var(--chart-1))",
+            },
+            mobile: {
+              label: "Mobile",
+              color: "hsl(var(--chart-2))",
+            },
+          }}
+          datakeys={["desktop", "mobile"]}
+          stacked={false}
+        />
+        <LineChartC
+          title='Line Chart - Single'
+          timeframe='January - June 2024'
+          chartData={[
+            { month: "January", desktop: 186, mobile: 93 },
+            { month: "February", desktop: 305, mobile: 552 },
+            { month: "March", desktop: 237, mobile: 118 },
+            { month: "April", desktop: 73, mobile: 36 },
+            { month: "May", desktop: 209, mobile: 104 },
+            { month: "June", desktop: 214, mobile: 107 },
+          ]}
+          chartConfig={{
+            desktop: {
+              label: "Desktop",
+              color: "hsl(var(--chart-1))",
+            },
+            mobile: {
+              label: "Mobile",
+              color: "hsl(var(--chart-2))",
+            },
+          }}
+          datakeys={["desktop", "mobile"]}
+        />
         <RadialChartC
+          title='Line Chart - Multiple'
+          timeframe='January - June 2024'
+          chartData={[
+            { title: "chrome", amount: 275, fill: "var(--color-yellow)" },
+            { title: "safari", amount: 200, fill: "var(--color-safari)" },
+            { title: "firefox", amount: 287, fill: "var(--color-firefox)" },
+          ]}
+          chartConfig={{
+            iamount: {
+              label: "Amount",
+            },
+            chrome: {
+              label: "Chrome",
+              color: "hsl(var(--chart-1))",
+            },
+            safari: {
+              label: "Safari",
+              color: "hsl(var(--chart-2))",
+            },
+            firefox: {
+              label: "Firefox",
+              color: "hsl(var(--chart-3))",
+            },
+          }}
+        />
+        <PieChartC
           title='Line Chart - Multiple'
           timeframe='January - June 2024'
           chartData={[
