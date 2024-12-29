0e9f9f4bbf4f9d0caa6b654ed1d50b4d48a00910
brooksolomon <officialbrooksolo@gmail.com>
2024-12-29 16:37:28 +0300

    added vertical bar chart

---
+++ resources/js/Components/HomeComponents/VerticalBarChartC.tsx
@@ -0,0 +1,68 @@
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+interface ChartDataInterface {
+  title: string;
+  amount: number;
+  fill: string;
+}
+
+interface VerticalBarChartCProps {
+  title?: string;
+  chartData: ChartDataInterface[];
+  chartConfig: ChartConfig;
+}
+
+export const VerticalBarChartC = ({
+  title = "",
+  chartData,
+  chartConfig,
+}: VerticalBarChartCProps) => {
+  return (
+    <Card className='flex items-center justify-center'>
+      <CardContent className=''>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout='vertical'
+            margin={{
+              left: 0,
+            }}
+          >
+            <YAxis
+              dataKey='title'
+              type='category'
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) =>
+                String(chartConfig[value as keyof typeof chartConfig]?.label)
+              }
+            />
+            <XAxis dataKey='amount' type='number' hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey='amount' layout='vertical' radius={5} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ resources/js/Pages/Finance.tsx
@@ -0,0 +1,300 @@
+import DefaultLayout from "@/Layouts/DefaultLayout";
+import { PageProps } from "@/types";
+import { Head, Link } from "@inertiajs/react";
+import { LineChartC } from "@/components/HomeComponents/LineChartC";
+import { BarChartC } from "@/components/HomeComponents/BarChartC";
+import { PieChartC } from "@/components/HomeComponents/PieChartC";
+import { RadialChartC } from "@/components/HomeComponents/RadialChartC";
+import { AreaChartC } from "@/components/HomeComponents/AreaChartC";
+import { CardSmallC } from "@/components/HomeComponents/CardSmallC";
+import { ResizableC } from "@/components/HomeComponents/ResizableC";
+import { HeatMapC } from "@/components/HomeComponents/HeatMapC";
+import { TableC } from "@/components/HomeComponents/TableC";
+import { VerticalBarChartC } from "@/components/HomeComponents/VerticalBarChartC";
+
+export default function Welcome({
+  auth,
+  laravelVersion,
+  phpVersion,
+}: PageProps<{ laravelVersion: string; phpVersion: string }>) {
+  const handleImageError = () => {
+    document.getElementById("screenshot-container")?.classList.add("!hidden");
+    document.getElementById("docs-card")?.classList.add("!row-span-1");
+    document.getElementById("docs-card-content")?.classList.add("!flex-row");
+    document.getElementById("background")?.classList.add("!hidden");
+  };
+
+  return (
+    <>
+      <DefaultLayout>
+        <Head title='Welcome' />
+
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3 p-2'>
+          <CardSmallC
+            title='Total Income'
+            value='$10,000'
+            startColor='#084923'
+            endColor='#35E980'
+          />
+          <CardSmallC
+            title='Total Expense'
+            value='$6,000'
+            startColor='#620410'
+            endColor='#F72642'
+          />
+          <CardSmallC
+            title='Net profit'
+            value='$4,000'
+            startColor='#001F66'
+            endColor='#477EFF'
+          />
+        </div>
+
+        <div className='grid md:grid-cols-[2fr,1fr] gap-4 p-2'>
+          <AreaChartC
+            title='Area Chart'
+            description='January - June 2024'
+            chartData={[
+              { date: "2024-12-01", car: 222, mobile: 150, tablet: 180 },
+              { date: "2024-12-02", car: 97, mobile: 180, tablet: 120 },
+              { date: "2024-12-12", car: 167, mobile: 120, tablet: 150 },
+              { date: "2024-12-12", car: 242, mobile: 260, tablet: 200 },
+              { date: "2024-12-05", car: 373, mobile: 290, tablet: 250 },
+              { date: "2024-12-06", car: 301, mobile: 340, tablet: 300 },
+              { date: "2024-12-07", car: 245, mobile: 180, tablet: 170 },
+              { date: "2024-12-08", car: 409, mobile: 320, tablet: 280 },
+              { date: "2024-12-09", car: 59, mobile: 110, tablet: 90 },
+              { date: "2024-12-10", car: 261, mobile: 190, tablet: 220 },
+              { date: "2024-12-11", car: 327, mobile: 350, tablet: 300 },
+              { date: "2024-12-12", car: 292, mobile: 210, tablet: 180 },
+              { date: "2024-12-13", car: 342, mobile: 380, tablet: 320 },
+              { date: "2024-12-14", car: 137, mobile: 220, tablet: 150 },
+              { date: "2024-12-15", car: 120, mobile: 170, tablet: 110 },
+              { date: "2024-12-16", car: 138, mobile: 190, tablet: 130 },
+              { date: "2024-12-17", car: 446, mobile: 360, tablet: 380 },
+              { date: "2024-12-18", car: 364, mobile: 410, tablet: 350 },
+              { date: "2024-12-19", car: 243, mobile: 180, tablet: 200 },
+              { date: "2024-12-20", car: 89, mobile: 150, tablet: 110 },
+              { date: "2024-12-21", car: 137, mobile: 200, tablet: 140 },
+              { date: "2024-12-22", car: 224, mobile: 170, tablet: 180 },
+              { date: "2024-12-23", car: 138, mobile: 230, tablet: 160 },
+              { date: "2024-12-24", car: 387, mobile: 290, tablet: 300 },
+              { date: "2024-12-25", car: 215, mobile: 250, tablet: 220 },
+              { date: "2024-12-26", car: 75, mobile: 130, tablet: 100 },
+              { date: "2024-12-27", car: 383, mobile: 420, tablet: 360 },
+              { date: "2024-12-28", car: 122, mobile: 180, tablet: 150 },
+              { date: "2024-12-29", car: 315, mobile: 240, tablet: 260 },
+              { date: "2024-12-30", car: 454, mobile: 380, tablet: 400 },
+              { date: "2024-05-01", car: 165, mobile: 220, tablet: 180 },
+              { date: "2024-05-02", car: 293, mobile: 310, tablet: 250 },
+              { date: "2024-05-03", car: 247, mobile: 190, tablet: 200 },
+              { date: "2024-05-04", car: 385, mobile: 420, tablet: 360 },
+              { date: "2024-05-05", car: 481, mobile: 390, tablet: 400 },
+              { date: "2024-05-06", car: 498, mobile: 520, tablet: 450 },
+              { date: "2024-05-07", car: 388, mobile: 300, tablet: 280 },
+              { date: "2024-05-08", car: 149, mobile: 210, tablet: 160 },
+              { date: "2024-05-09", car: 227, mobile: 180, tablet: 190 },
+              { date: "2024-05-10", car: 293, mobile: 330, tablet: 270 },
+              { date: "2024-05-11", car: 335, mobile: 270, tablet: 250 },
+              { date: "2024-05-12", car: 197, mobile: 240, tablet: 200 },
+              { date: "2024-05-13", car: 197, mobile: 160, tablet: 140 },
+              { date: "2024-05-14", car: 448, mobile: 490, tablet: 420 },
+              { date: "2024-05-15", car: 473, mobile: 380, tablet: 350 },
+              { date: "2024-05-16", car: 338, mobile: 400, tablet: 320 },
+              { date: "2024-05-17", car: 499, mobile: 420, tablet: 380 },
+              { date: "2024-05-18", car: 315, mobile: 350, tablet: 300 },
+              { date: "2024-05-19", car: 235, mobile: 180, tablet: 160 },
+              { date: "2024-05-20", car: 177, mobile: 230, tablet: 190 },
+              { date: "2024-05-21", car: 82, mobile: 140, tablet: 110 },
+              { date: "2024-05-22", car: 81, mobile: 120, tablet: 100 },
+              { date: "2024-05-23", car: 252, mobile: 290, tablet: 240 },
+              { date: "2024-05-24", car: 294, mobile: 220, tablet: 250 },
+              { date: "2024-05-25", car: 201, mobile: 250, tablet: 220 },
+              { date: "2024-05-26", car: 213, mobile: 170, tablet: 180 },
+              { date: "2024-05-27", car: 420, mobile: 460, tablet: 400 },
+              { date: "2024-05-28", car: 233, mobile: 190, tablet: 200 },
+              { date: "2024-05-29", car: 78, mobile: 130, tablet: 110 },
+              { date: "2024-05-30", car: 340, mobile: 280, tablet: 300 },
+              { date: "2024-05-31", car: 178, mobile: 230, tablet: 200 },
+              { date: "2024-06-01", car: 178, mobile: 200, tablet: 180 },
+              { date: "2024-06-02", car: 470, mobile: 410, tablet: 360 },
+              { date: "2024-06-03", car: 103, mobile: 160, tablet: 140 },
+              { date: "2024-06-04", car: 439, mobile: 380, tablet: 350 },
+              { date: "2024-06-05", car: 88, mobile: 140, tablet: 120 },
+              { date: "2024-06-06", car: 294, mobile: 250, tablet: 220 },
+              { date: "2024-06-07", car: 323, mobile: 370, tablet: 320 },
+              { date: "2024-06-08", car: 385, mobile: 320, tablet: 280 },
+              { date: "2024-06-09", car: 438, mobile: 480, tablet: 420 },
+              { date: "2024-06-10", car: 155, mobile: 200, tablet: 180 },
+              { date: "2024-06-11", car: 92, mobile: 150, tablet: 130 },
+              { date: "2024-06-12", car: 492, mobile: 420, tablet: 380 },
+              { date: "2024-06-13", car: 81, mobile: 130, tablet: 110 },
+              { date: "2024-06-14", car: 426, mobile: 380, tablet: 340 },
+              { date: "2024-06-15", car: 307, mobile: 350, tablet: 300 },
+              { date: "2024-06-16", car: 371, mobile: 310, tablet: 270 },
+              { date: "2024-06-17", car: 475, mobile: 520, tablet: 460 },
+              { date: "2024-06-18", car: 107, mobile: 170, tablet: 150 },
+              { date: "2024-06-19", car: 341, mobile: 290, tablet: 250 },
+              { date: "2024-06-20", car: 408, mobile: 450, tablet: 400 },
+              { date: "2024-06-21", car: 169, mobile: 210, tablet: 180 },
+              { date: "2024-06-22", car: 317, mobile: 270, tablet: 240 },
+              { date: "2024-06-23", car: 480, mobile: 530, tablet: 470 },
+              { date: "2024-06-24", car: 132, mobile: 180, tablet: 160 },
+              { date: "2024-06-25", car: 141, mobile: 190, tablet: 170 },
+              { date: "2024-06-26", car: 434, mobile: 380, tablet: 340 },
+              { date: "2024-06-27", car: 448, mobile: 490, tablet: 430 },
+              { date: "2024-06-28", car: 149, mobile: 200, tablet: 180 },
+              { date: "2024-06-29", car: 103, mobile: 160, tablet: 140 },
+              { date: "2024-06-30", car: 446, mobile: 400, tablet: 360 },
+            ]}
+            chartConfig={{
+              amount: {
+                label: "Amount",
+              },
+              car: {
+                label: "Car",
+                color: "hsl(var(--chart-1))",
+              },
+              mobile: {
+                label: "Mobile",
+                color: "hsl(var(--chart-2))",
+              },
+              tablet: {
+                label: "Tablet",
+                color: "hsl(var(--chart-3))",
+              },
+            }}
+            datakeys={["car", "mobile", "tablet"]}
+          />
+          <PieChartC
+            title='Line Chart - Multiple'
+            timeframe='January - June 2024'
+            chartData={[
+              { title: "chrome", amount: 275, fill: "var(--color-chrome)" },
+              { title: "safari", amount: 200, fill: "var(--color-safari)" },
+              { title: "firefox", amount: 287, fill: "var(--color-firefox)" },
+            ]}
+            chartConfig={{
+              iamount: {
+                label: "Amount",
+              },
+              chrome: {
+                label: "Chrome",
+                color: "hsl(var(--chart-1))",
+              },
+              safari: {
+                label: "Safari",
+                color: "hsl(var(--chart-2))",
+              },
+              firefox: {
+                label: "Firefox",
+                color: "hsl(var(--chart-3))",
+              },
+            }}
+          />
+        </div>
+        <ResizableC />
+        <HeatMapC
+          value={[
+            { date: "2024/01/11", count: 2 },
+            { date: "2024/01/12", count: 20 },
+            { date: "2024/01/13", count: 10 },
+            { date: "2024/04/11", count: 2 },
+            { date: "2024/05/01", count: 5 },
+            { date: "2024/05/02", count: 5 },
+            { date: "2022/05/04", count: 11 },
+            { date: "2023/05/05", count: 5 },
+          ]}
+        />
+        <div className='grid grid-cols-[2fr,1fr] gap-10'>
+          <TableC
+            datakeys={[
+              "invoice",
+              "paymentStatus",
+              "totalAmount",
+              "paymentMethod",
+            ]}
+            value={[
+              {
+                invoice: "INV001",
+                paymentStatus: "Paid",
+                totalAmount: "$250.00",
+                paymentMethod: "Credit Card",
+              },
+              {
+                invoice: "INV002",
+                paymentStatus: "Pending",
+                totalAmount: "$150.00",
+                paymentMethod: "PayPal",
+              },
+              {
+                invoice: "INV003",
+                paymentStatus: "Unpaid",
+                totalAmount: "$350.00",
+                paymentMethod: "Bank Transfer",
+              },
+              {
+                invoice: "INV004",
+                paymentStatus: "Paid",
+                totalAmount: "$450.00",
+                paymentMethod: "Credit Card",
+              },
+              {
+                invoice: "INV005",
+                paymentStatus: "Paid",
+                totalAmount: "$550.00",
+                paymentMethod: "PayPal",
+              },
+              {
+                invoice: "INV006",
+                paymentStatus: "Pending",
+                totalAmount: "$200.00",
+                paymentMethod: "Bank Transfer",
+              },
+              {
+                invoice: "INV007",
+                paymentStatus: "Unpaid",
+                totalAmount: "$300.00",
+                paymentMethod: "Credit Card",
+              },
+            ]}
+          />
+          <VerticalBarChartC
+            chartData={[
+              { title: "chrome", amount: 275, fill: "var(--color-chrome)" },
+              { title: "safari", amount: 200, fill: "var(--color-safari)" },
+              { title: "firefox", amount: 187, fill: "var(--color-firefox)" },
+              { title: "edge", amount: 173, fill: "var(--color-edge)" },
+              { title: "other", amount: 90, fill: "var(--color-other)" },
+            ]}
+            chartConfig={{
+              visitors: {
+                label: "Visitors",
+              },
+              chrome: {
+                label: "Chrome",
+                color: "hsl(var(--chart-1))",
+              },
+              safari: {
+                label: "Safari",
+                color: "hsl(var(--chart-2))",
+              },
+              firefox: {
+                label: "Firefox",
+                color: "hsl(var(--chart-3))",
+              },
+              edge: {
+                label: "Edge",
+                color: "hsl(var(--chart-4))",
+              },
+              other: {
+                label: "Other",
+                color: "hsl(var(--chart-5))",
+              },
+            }}
+          />
+        </div>
+      </DefaultLayout>
+    </>
+  );
+}
