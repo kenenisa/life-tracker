0791128f3e6919a8b19b31b9942ea912dd4dc2a1
brooksolomon <officialbrooksolo@gmail.com>
2024-12-29 20:54:41 +0300

    added weeekday card

---
+++ resources/js/Components/HomeComponents/WeekDaysTabC.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { WorkoutCardC } from "@/components/HomeComponents/WorkoutCardC";
+interface WorkoutDataInterface {
+  day: string;
+  category: string;
+  title: string;
+  text: string;
+}
+interface WeekDaysTabCProps {
+  workoutData: WorkoutDataInterface[];
+}
+
+export const WeekDaysTabC = ({ workoutData }: WeekDaysTabCProps) => {
+  return (
+    <Tabs defaultValue='mon' className='mx-12 my-4'>
+      <TabsList className='grid w-full grid-cols-7'>
+        <TabsTrigger value='mon'>Mon</TabsTrigger>
+        <TabsTrigger value='tue'>Tue</TabsTrigger>
+        <TabsTrigger value='wed'>Wed</TabsTrigger>
+        <TabsTrigger value='thu'>Thu</TabsTrigger>
+        <TabsTrigger value='fri'>Fri</TabsTrigger>
+        <TabsTrigger value='sat'>Sat</TabsTrigger>
+        <TabsTrigger value='sun'>Sun</TabsTrigger>
+      </TabsList>
+      {daysOfWeek.map((day) => (
+        <TabsContent key={day} value={day}>
+          <Card>
+            <CardContent className='p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-4 gap-12'>
+              {/* Filter workouts for the current day */}
+              {workoutData
+                .filter((workout) => workout.day === day)
+                .map((workout, index) => (
+                  <WorkoutCardC
+                    key={index}
+                    category={workout.category}
+                    title={workout.title}
+                    text={workout.text}
+                  />
+                ))}
+            </CardContent>
+            <CardFooter className='flex justify-end'>
+              <Button>Add workout</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+};
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
