f17d67f8bf26e1a4c28ee94ad5c72c53d22bc539
brooksolomon <officialbrooksolo@gmail.com>
2024-12-29 08:17:04 +0300

    lineChart with multiple time frames complete

---
+++ resources/js/Components/HomeComponents/AreaChartC.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import * as React from "react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/Components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/Components/ui/chart";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/Components/ui/select";
+
+interface ChartDataInterface {
+  date: string;
+  [key: string]: number | string;
+}
+interface AreaChartCProps {
+  title: string;
+  timeframe: string;
+  chartData: ChartDataInterface[];
+  chartConfig: ChartConfig;
+  datakeys: string[];
+}
+
+export const AreaChartC = ({
+  title,
+  timeframe,
+  chartData,
+  chartConfig,
+  datakeys,
+}: AreaChartCProps) => {
+  const [timeRange, setTimeRange] = React.useState("7d");
+
+  const filteredData = chartData.filter((item) => {
+    const date = new Date(item.date);
+    const referenceDate = new Date("2024-06-30");
+    let daysToSubtract = 90;
+    if (timeRange === "30d") {
+      daysToSubtract = 30;
+    } else if (timeRange === "7d") {
+      daysToSubtract = 7;
+    } else if (timeRange === "1d") {
+      daysToSubtract = 1;
+    }
+    const startDate = new Date(referenceDate);
+    startDate.setDate(startDate.getDate() - daysToSubtract);
+    return date >= startDate;
+  });
+
+  return (
+    <Card>
+      <CardHeader className='flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row'>
+        <div className='grid flex-1 gap-1 text-center sm:text-left'>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>
+            Showing total visitors for the last 3 months
+          </CardDescription>
+        </div>
+        <Select value={timeRange} onValueChange={setTimeRange}>
+          <SelectTrigger
+            className='w-[160px] rounded-lg sm:ml-auto'
+            aria-label='Select a value'
+          >
+            <SelectValue placeholder='Last 3 months' />
+          </SelectTrigger>
+          <SelectContent className='rounded-xl'>
+            <SelectItem value='90d' className='rounded-lg'>
+              Last 3 months
+            </SelectItem>
+            <SelectItem value='30d' className='rounded-lg'>
+              Last 30 days
+            </SelectItem>
+            <SelectItem value='7d' className='rounded-lg'>
+              Last 7 days
+            </SelectItem>
+            <SelectItem value='1d' className='rounded-lg'>
+              Last 1 day
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent className='px-2 pt-4 sm:px-6 sm:pt-6'>
+        <ChartContainer
+          config={chartConfig}
+          className='aspect-auto h-[250px] w-full'
+        >
+          <AreaChart data={filteredData}>
+            <defs>
+              {datakeys.map((item, index) => (
+                <linearGradient id={`fill${item}`} x1='0' y1='0' x2='0' y2='1'>
+                  <stop
+                    offset='5%'
+                    stopColor={`var(--color-${item})`}
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset='95%'
+                    stopColor={`var(--color-${item})`}
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+              ))}
+            </defs>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey='date'
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value);
+                return date.toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                });
+              }}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                    });
+                  }}
+                  indicator='dot'
+                />
+              }
+            />
+            {datakeys.map((item, index) => (
+              <Area
+                dataKey={item}
+                type='natural'
+                fill={`url(#fill${item})`}
+                stroke={`var(--color-${item})`}
+                stackId='a'
+              />
+            ))}
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
