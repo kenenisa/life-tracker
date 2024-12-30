4921905ecb7a7a16cfa48ca6a4976a3f67412604
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2024-12-30 10:47:12 +0300

    fix: removed bread smth

--- resources/js/Layouts/DefaultLayout.tsx
+++ resources/js/Layouts/DefaultLayout.tsx
@@ -23,19 +23,19 @@ export default function DefaultLayout({ children }: any) {
                     <div className='flex items-center gap-2 px-4'>
                         <SidebarTrigger className='-ml-1' />
                         <Separator orientation='vertical' className='mr-2 h-4' />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className='hidden md:block'>
-                                    <BreadcrumbLink href='#'>
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className='hidden md:block' />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
+                        {/* <Breadcrumb> */}
+                        {/*     <BreadcrumbList> */}
+                        {/*         <BreadcrumbItem className='hidden md:block'> */}
+                        {/*             <BreadcrumbLink href='#'> */}
+                        {/*                 Building Your Application */}
+                        {/*             </BreadcrumbLink> */}
+                        {/*         </BreadcrumbItem> */}
+                        {/*         <BreadcrumbSeparator className='hidden md:block' /> */}
+                        {/*         <BreadcrumbItem> */}
+                        {/*             <BreadcrumbPage>Data Fetching</BreadcrumbPage> */}
+                        {/*         </BreadcrumbItem> */}
+                        {/*     </BreadcrumbList> */}
+                        {/* </Breadcrumb> */}
                     </div>
                 </header>
                 {children}
