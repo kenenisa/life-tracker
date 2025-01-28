fc884f385f6e94bd68fffe794fc9878866cd37b3
LeulAshenafi <140915176+LeulAshenafi@users.noreply.github.com>
2025-01-28 08:33:28 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+//is should be autheticated
     /**
      * The attributes that should be hidden for serialization.
      *
